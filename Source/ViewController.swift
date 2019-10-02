5e830e7db7a69680d81b8be97f5190a92a5bccb1
Jin Khera <jin.khera@gmail.com>
2019-10-02 15:33:36 +0100

    create base project

---
+++ Source/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BBC News App
+//
+//  Created by Jatinder Pal Singh Khera on 02/10/2019.
+//  Copyright © 2019 Jatinder Pal Singh Khera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
